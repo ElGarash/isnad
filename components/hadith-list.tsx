ec1404cf07c716fc71a956b58c16b29bc010fd8a
Abdulrhmn Ghanem <abdoghanem160@gmail.com>
2025-02-12 03:55:17 +0000

    Link to hadith page

--- components/hadith-list.tsx
+++ components/hadith-list.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Hadith } from "@/lib/sqlite";
+import Link from "next/link";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import { Card } from "@/components/ui/card";
 import { useRef } from "react";
@@ -46,39 +47,41 @@ export default function HadithList({ hadiths }: HadithListProps) {
                   transform: `translateY(${virtualRow.start}px)`,
                 }}
               >
-                <Card
-                  className="relative border-4 border-black overflow-hidden m-2"
-                >
-                  <div className="absolute top-0 left-0 w-24 h-24 bg-yellow-400 transform -rotate-45 -translate-x-12 -translate-y-12 z-10"></div>
-                  <div className="absolute top-2 left-2 z-20 transform rotate-45">
-                    <div className="bg-black text-yellow-400  text-xl font-bold px-2 py-1 transform -rotate-90">
-                      {hadith.id}
-                    </div>
-                  </div>
-                  <div className="p-6 pt-10 pl-16">
-                    <div className="text-right mb-4">
-                      <h3 className="font-bold text-xl mb-1 inline-block bg-black text-white px-2 py-1 transform -skew-x-12">
-                        { }
-                      </h3>
-                      <div className="text-sm text-gray-600 mt-2">
-                        <span className="inline-block bg-gray-200 px-2 py-1 ml-2 mb-2">
-                          {hadith.source}
-                        </span>
-                        <span className="inline-block bg-gray-200 px-2 py-1">
-                          {hadith.chapter}
-                        </span>
+                <Link href={`/hadith/${encodeURIComponent(hadith.source)}/${hadith.chapter_no}/${hadith.hadith_no}`} hrefLang="ar">
+                  <Card
+                    className="relative border-4 border-black overflow-hidden m-2"
+                  >
+                    <div className="absolute top-0 left-0 w-24 h-24 bg-parchment transform -rotate-45 -translate-x-12 -translate-y-12 z-10"></div>
+                    <div className="absolute top-2 left-2 z-20 transform rotate-45">
+                      <div className="bg-black text-parchment  text-xl font-bold px-2 py-1 transform -rotate-90">
+                        {hadith.id}
                       </div>
                     </div>
-                    <div className="relative">
-                      <div className="absolute right-0 top-0 bottom-0 w-1 bg-black"></div>
-                      <p className="text-right leading-relaxed whitespace-pre-wrap break-words pr-4">
-                        {hadith.text_ar}
-                      </p>
+                    <div className="p-6 pt-10 pl-16">
+                      <div className="text-right mb-4">
+                        <h3 className="font-bold text-xl mb-1 inline-block bg-black text-white px-2 py-1 transform -skew-x-12">
+                          { }
+                        </h3>
+                        <div className="text-sm text-gray-600 mt-2">
+                          <span className="inline-block bg-gray-200 px-2 py-1 ml-2 mb-2">
+                            {hadith.source}
+                          </span>
+                          <span className="inline-block bg-gray-200 px-2 py-1">
+                            {hadith.chapter}
+                          </span>
+                        </div>
+                      </div>
+                      <div className="relative">
+                        <div className="absolute right-0 top-0 bottom-0 w-1 bg-black"></div>
+                        <p className="text-right leading-relaxed whitespace-pre-wrap break-words pr-4">
+                          {hadith.text_ar}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="absolute bottom-0 left-0 w-full h-1 bg-black"></div>
-                  <div className="absolute bottom-0 right-0 w-1 h-full bg-black"></div>
-                </Card>
+                    <div className="absolute bottom-0 left-0 w-full h-1 bg-black"></div>
+                    <div className="absolute bottom-0 right-0 w-1 h-full bg-black"></div>
+                  </Card>
+                </Link>
               </div>
             );
           })}
